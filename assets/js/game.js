a571a56007cd8a0ca757fbdee7912bd8f4c00f3c
Brynne Eastman <brynne.eastman@gmail.com>
2021-09-02 07:47:07 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,8 +33,10 @@ var fight = function(enemyName) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -52,8 +54,11 @@ var fight = function(enemyName) {
           window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
         }
     
-        // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
           enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -87,7 +92,14 @@ var fight = function(enemyName) {
           var pickedEnemyName = enemyNames[i];
     
           // reset enemyHealth before starting new fight
-          enemyHealth = 50;
+          
+          enemyHealth = randomNumber(max, min);
+
+          var randomNumber = function(max, min) {
+            var value = Math.floor(Math.random() * (21) + 40);
+
+            return value;
+          };
     
           // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
           fight(pickedEnemyName);
